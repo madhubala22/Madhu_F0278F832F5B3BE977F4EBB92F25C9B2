d21c91d89c54d7acd611a8a4906a64ea05380aee
madhubala22 <149038767+madhubala22@users.noreply.github.com>
2023-10-26 15:25:40 +0530

    Add files via upload

---
+++ challenge 2.2.py
@@ -0,0 +1,13 @@
+class player:
+  def play(self):
+    print("The Player is playing cricket.")
+class batsman(player):
+  def play(self):
+    print("The batsman is batting.")
+class bowler(player): 
+  def play(self):
+    print("The bowler is bowler.")
+Batsman=batsman()
+Bowler=bowler()
+Batsman.play()
+Bowler.play()
